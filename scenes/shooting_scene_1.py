a5d0aa4eaf48e1cd9851b576c03af8291e2d6c50
MAAAARCY <masatarou.1003@icloud.com>
2024-02-20 11:44:52 +0900

    Add:special_bullet.py

--- scenes/shooting_scene_1.py
+++ scenes/shooting_scene_1.py
@@ -47,7 +47,7 @@ class Shooting_scene_1:
                     if event.key == K_SPACE:
                         player.shot_bullet()  # プレイヤーの弾の発射処理
                         mp.value += 1
-                    if event.key == K_a and mp.value > 20:
+                    if event.key == K_a and mp.value >= 20:
                         player.special_bullet()
                         mp.value = max(mp.value - 20, 0)
 
